4d3f8b95a52492248f072eeb4562c31b78e4cc71
marcosmvidase <marcosmvidas@gmail.com>
2024-10-11 14:58:43 -0300

    :gear: Reconfiguração do swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,30 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ *  @OA\Info(
+ *      version="1.0.0",
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="exemplo@exemplo.com.br",
+ *          url="https://esocialbrasil.com.br/"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ *  )
+ *
+ *  @OA\Get(
+ *      path="/",
+ *      description="Home page",
+ *      @OA\Response(response="default", description="Benvindo ao site")
+ * )
+ *
+ *
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
